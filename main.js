0a5252705ca8538c2baea314595d63149fcea446
nicke <nicken333@gmail.com>
2021-01-27 16:03:12 +0100

    test1

---
+++ main.js
@@ -0,0 +1,31 @@
+console.log('dw');
+
+async function readPetsJson () {
+    let jsonPets = await $.getJSON("pets.json");
+    console.log(jsonPets);
+
+    showPetsAsHtml(jsonPets)
+}
+
+async function readPersonsJson () {
+    let jsonPersons = await $.getJSON("persons.json");
+    console.log(jsonPersons);
+}
+
+
+
+
+function showPetsAsHtml (jsonPets) {
+    for (let pet of jsonPets) {
+        let $pet = $('<div class="pets"></div>'); 
+
+        for (let key in pet) {
+            let value = pet[key];
+            $pet.append('<div><span>' + key + ': <span>' + value + '</div>');
+        }
+        $('body').append($pet);
+        $('body').append("<br>");
+    }
+}
+
+readPetsJson();
